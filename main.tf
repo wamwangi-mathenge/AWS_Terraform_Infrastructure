4f43644e85c7b6ff5c1c434d43394a09050c1df4
Brian <wamwangimathenge@gmail.com>
2023-11-17 11:30:35 +0300

    Create a VPC

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "vpc_infrastructure" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "infra_vpc"
+  }
+}

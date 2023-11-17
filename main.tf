9b40d1339fbe9efe783985bd6b882c419ecfd777
Brian <wamwangimathenge@gmail.com>
2023-11-17 11:37:59 +0300

    Create a second subnet

--- main.tf
+++ main.tf
@@ -20,14 +20,25 @@ resource "aws_vpc" "vpc_infrastructure" {
   }
 }
 
-# Create a Subnet
+# Create the first Subnet
 resource "aws_subnet" "infra_public_subnet" {
   vpc_id = aws_vpc.vpc_infrastructure.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "infra_public_subnet"
+    Name = "infra_public_subnet1"
+  }
+}
+
+# Create the second subnet
+resource "aws_subnet" "infra_public_subnet2" {
+  vpc_id = aws_vpc.vpc_infrastructure.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "infra_public_subnet2"
   }
 }
 

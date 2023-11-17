f1a9c97854e7ca6a2e618ab7d5f92c8c5fcd485f
Brian <wamwangimathenge@gmail.com>
2023-11-17 11:35:27 +0300

    Create the subnet in an AZ

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_vpc" "vpc_infrastructure" {
 resource "aws_subnet" "infra_public_subnet" {
   vpc_id = aws_vpc.vpc_infrastructure.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "infra_public_subnet"
